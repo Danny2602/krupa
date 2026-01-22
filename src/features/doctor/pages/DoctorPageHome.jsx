2b1945b0664694d520d9782d49ac4f8ecd21c8d5
Danny Simba <dannysimba2602@gmail.com>
2026-01-21 19:48:18 -0500

    Add home.jsx fir doctor section

--- src/features/doctor/pages/DoctorPageHome.jsx
+++ src/features/doctor/pages/DoctorPageHome.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { WelcomeBanner } from '../components/WelcomeBanner'
+import { useAuth } from '@/context/AuthContext';
+import { Typography } from '@mui/material';
 export default function DoctorPageHome() {
+    const { user } = useAuth()
     return (
-        <div>DoctorPageHome</div>
+        <>
+            <WelcomeBanner user={user} />
+            <Typography variant="h5" className="font-bold text-gray-800 mb-4">
+                Citas Medicas Pendientes de confirmación
+            </Typography>
+        </>
     )
 }
 
